1c552e1c485910fad5ee0d308d3c1a6852f60d74
My_name <me@example.com>
2021-12-08 17:09:19 +0200

    The linked list reverse function with pointers and the main method

--- 2.LinkedLists/linkedList_reverse.py
+++ 2.LinkedLists/linkedList_reverse.py
@@ -1,5 +1,8 @@
 # Interview Question #8
 # Construct an in-place algorithm to reverse a linked list!
+# Pointer method that achieves in-place algorithm with O(N) running time complexity
+# 3 pointers: previous_node, next_node, current_node
+
 
 class Node:
 
@@ -13,7 +16,23 @@ class LinkedList:
         self.head = None
         self.size = 0
 
-    def insert(self, data):
+    # O(N) running time complexity
+    def reverse_list(self):
+        ''' Reverse the linked list using three pointers.'''
+
+        current_node = self.head
+        previous_node = None
+        next_node = None
+
+        while current_node is not None:
+            next_node = current_node.next_node
+            current_node.next_node = previous_node  # The moment the reference is flipped
+            previous_node = current_node
+            current_node = next_node
+
+        self.head = previous_node
+
+    def insert_start(self, data):
         self.size += 1
         new_node = Node(data)
 
@@ -23,9 +42,27 @@ class LinkedList:
             new_node.next_node = self.head
             self.head = new_node
 
-    def traverse(self):
+    def traverse_list(self):
         actual_node = self.head
-
         while actual_node is not None:
             print("%d" % actual_node.data)
             actual_node = actual_node.next_node
+
+if __name__ == "__main__":
+
+    linked_list = LinkedList()
+
+    linked_list.insert_start(12)
+    linked_list.insert_start(122)
+    linked_list.insert_start(3)
+    linked_list.insert_start(31)
+    linked_list.insert_start(10)
+    linked_list.insert_start(11)
+
+    print("\nNormal list: ")
+    linked_list.traverse_list()
+    linked_list.reverse_list()
+
+    print("\nReversed list: ")
+    linked_list.traverse_list()
+
